46c7b5a8ee5a6cec7e1d06f55340516328e7615a
mdc004 <91119831+mdc004@users.noreply.github.com>
2024-11-19 10:13:39 +0100

    Delete lab algo directory

--- lab algo/lab1/prog2.c
+++
@@ -1,8 +0,0 @@
-#include "prog2.h"
-
-int main(void) {
-	int n_euro = geteuro();
-	double n_lire = n_euro * LIRE_PER_EURO;
-	putlire(n_euro, n_lire);
-	return 0;
-}
--- lab algo/lab1/prog2.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#define LIRE_PER_EURO (1936.27)
-
-int geteuro(void);
-void putlire(int, double);
